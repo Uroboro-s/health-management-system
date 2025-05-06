baacee21b74189ebd0cbf27bdf681c440e0d54dd
Janmeyjai Mishra <mishrajanmeyjai100@gmail.com>
2025-05-06 22:56:07 +0530

    AuthService basics done

--- AuthService/src/controllers/authController.ts
+++ AuthService/src/controllers/authController.ts
@@ -3,9 +3,11 @@ import User from "../models/User";
 import jwt from "jsonwebtoken";
 
 interface UserProps {
-  _id: string;
+  _id: unknown;
   role: string;
   email: string;
+  password: string;
+  __v: number;
 }
 
 function generateToken(user: UserProps): string {
@@ -19,15 +21,26 @@ function generateToken(user: UserProps): string {
 }
 
 async function register(req: Request, res: Response) {
+  console.log(req.body);
+
+  const { email, password } = req.body;
+
   try {
-    // const user = await User.create(req.body);
-    const user = {
-      _id: "1",
-      role: "admin",
-      email: "some@abc.com",
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(401).json({ message: "User already exists!" });
+    }
+    const user = await User.create(req.body);
+
+    const jwtUser = {
+      email: user.email,
+      password: user.password,
+      role: user.role,
+      _id: user._id,
+      __v: user.__v,
     };
-    const token = generateToken(user);
-    // const token = "amksd";
+
+    const token = generateToken(jwtUser);
     res.status(201).json({ token, user });
   } catch (err: any) {
     res.status(400).json({ error: err.message });
@@ -36,19 +49,23 @@ async function register(req: Request, res: Response) {
 
 async function login(req: Request, res: Response) {
   console.log(req.body);
-  //   const { email, password } = req.body;
+  const { email, password } = req.body;
   try {
-    // const user = await User.findOne({ email });
-    // if (!user || !(await user.comparePassword(password))) {
-    //   return res.status(401).json({ message: "Invalid credentials" });
-    // }
-    const user = {
-      _id: "1",
-      role: "admin",
-      email: "some@abc.com",
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const jwtUser = {
+      email: user.email,
+      password: user.password,
+      role: user.role,
+      _id: user._id,
+      __v: user.__v,
     };
-    const token = generateToken(user);
-    // let token = "x";
+
+    const token = generateToken(jwtUser);
+
     res.status(201).json({ token, user });
   } catch (err: any) {
     res.status(500).json({ error: err.message });
